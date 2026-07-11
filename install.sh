9f92a7021950e73b3284301e28fa249bef44cd82
Corey Merritt <coreyleemerritt@gmail.com>
2026-07-11 02:17:33 -0400

    Fix file permissions; Implement op templating

--- install.sh
+++ install.sh
@@ -3,10 +3,20 @@
 set -euo pipefail
 
 script_dir="$(dirname $(readlink -f $0))"
+cd $script_dir
 
-files="$(ls -A $script_dir | grep -v install.sh)"
-
+files="$(ls -A $script_dir | grep -v install.sh | grep -v .git)"
 for file in $files; do
   cp -r "${HOME}/${file}" "${HOME}/${file}.bak" 2>/dev/null || true
   cp -r "${script_dir}/${file}" "${HOME}/"
+  if [[ -d "$file" ]]; then
+    find "${HOME}/${file}" -name *.template -exec rm -rf {} \;
+  fi
+done
+
+template_files="$(find -name *.template)"
+for template_file_path in $template_files; do
+  clean_template_file_path="${template_file_path#./}"
+  clean_template_file_path="$(echo $clean_template_file_path | sed 's|.template||g')"
+  op inject -i "$template_file_path" -o "${HOME}/${clean_template_file_path}"
 done
